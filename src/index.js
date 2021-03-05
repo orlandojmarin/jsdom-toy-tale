2f32c4f1d86b57cf36cdb76d9f5ad7e1026e82c6
Orlando Marin <orlandojermainemarin@gmail.com>
2021-03-04 23:36:30 -0800

    completed assignment

--- src/index.js
+++ src/index.js
@@ -23,105 +23,76 @@ function getToyData() {
   .then(response => response.json())
   .then(data => {
     data.forEach(toy => {
-      console.log(toy)
-      const createDiv = document.createElement("div")
-      createDiv.setAttribute("class","card") 
-
-      const createH2 = document.createElement("h2")
-      createH2.textContent = toy.name
-
-      const createImg = document.createElement("img")
-      createImg.src = toy.image
-      createImg.setAttribute("class", "toy-avatar")
-
-      const createParagraph = document.createElement("p")
-      createParagraph.textContent = toy.likes
-
-      const createButton = document.createElement("button")
-      createButton.textContent = "Like <3"
-      createButton.setAttribute("class", "like-btn")
-      //createButton.addEventListener("click", (e) => updateLikes(e))
-
-      createDiv.append(createH2, createImg, createParagraph, createButton)
-      document.getElementById("toy-collection").append(createDiv)
+      displayToy(toy)
     }) 
   });
 }
 
-//add new toy function, include a POST request
-  //querySelector add either . or # depending on class or ID
-  //selecting the class form html element
+//CREATE CARD FOR EACH TOY
+function displayToy(toy) {
+  const createDiv = document.createElement("div")
+  createDiv.setAttribute("class","card") 
+
+  const createH2 = document.createElement("h2")
+  createH2.textContent = toy.name
+
+  const createImg = document.createElement("img")
+  createImg.src = toy.image
+  createImg.setAttribute("class", "toy-avatar")
+
+  const createParagraph = document.createElement("p")
+  createParagraph.textContent = toy.likes
+
+  const createButton = document.createElement("button")
+  
+  createButton.addEventListener("click", (e) => addMoreLikes(createParagraph, toy))
+  createButton.textContent = "Like <3"
+  createButton.setAttribute("class", "like-btn")
+
+  createDiv.append(createH2, createImg, createParagraph, createButton)
+  document.getElementById("toy-collection").append(createDiv)
+}
+
+//ADD LIKES VIA PATCH REQUEST
+function addMoreLikes(p, toy) {
+  console.log(p)
+  let updateLikes = parseInt(p.textContent) + 1
+  const addLikes = {likes: updateLikes}
+
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(addLikes)
+  }).then(response => response.json()).then(likes => p.textContent = updateLikes).catch((error) => {
+    console.error("Error:", error)
+  })
+}
+
+
+//ADD FUNCTIONALITY TO "CREATE NEW TOY" BUTTON
   const form = document.querySelector(".add-toy-form")
   form.addEventListener("submit", addNewToy)
-  function addNewToy() {
+  function addNewToy(e) {
     e.preventDefault()
-    console.log(e)
-  // POST http://localhost:3000/toys
-  // headers: 
-  // {
-  //   "Content-Type": "application/json",
-  //   Accept: "application/json"
-  // }
-  
-  // body: JSON.stringify({
-  //   "name": "Jessie",
-  //   "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-  //   "likes": 0
-  }
-
-
-// function updateLikes(e) {
-//     fetch(`http://localhost:3000/toys/${id}`, {
-//     method: 'PATCH', // or 'PUT'
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Accept: "application/json"
-//     },
-//     body: JSON.stringify({
-//       "likes": likes.value + 1
-//     }),
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log('Success:', data);
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
-//   }
-// }
-
-// function addNewToy() {
-// const createNewToy = document.querySelector(".submit")
-// createNewToy.addEventListener("click", (e) => {
-//   e.preventDefault()
-//   const makeNewToyName = document.querySelector('[name = "name"]')
-//   console.log(makeNewToyName)
-//   const makeNewToyImage = 
-//   fetch('http://localhost:3000/toys', {
-//   method: 'POST', // or 'PUT'
-//   headers: {
-//     'Content-Type': 'application/json',
-//     "Accept": "application/json"
-//   },
-//   body: JSON.stringify({
-//     name: ,
-//     image: ,
-//     likes: 
-//   }),
-// })
-// .then(response => response.json())
-// .then(data => {
-//   console.log('Success:', data);
-// })
-// .catch((error) => {
-//   console.error('Error:', error);
-// });
-// })
-// }
-
-// addNewToy()
-
-
-//get the number of likes to change +1 each time the like button is clicked
-//make sure that the add a toy feature works
+    const name = document.querySelector('[name = "name"]').value
+    const image = document.querySelector('[name = "image"]').value
+    //CREATE TOY OBJECT BASED OFF INFO ADDED TO THE FORM
+    //SEND FORM INFO INTO THE DATABASE
+    //UPDATED INFO FROM DATABASE DISPLAYS ON THE SCREEN
+    console.log(name, image)
+    const toy = {
+      name: name,
+      image: image,
+      likes: 0
+    }
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(toy)
+    }
+    fetch("http://localhost:3000/toys", config).then(response => response.json()).then(toy => displayToy(toy)).catch((error) => {console.error("Error:", error)})
+  } 
